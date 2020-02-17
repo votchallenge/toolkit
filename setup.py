9cdd864d389df2e1906c8672877aa384d34406b6
Luka Čehovin <luka.cehovin@gmail.com>
2020-02-17 13:15:50 +0100

    Readme fixes, setup.py updates with description and dependencies.

--- setup.py
+++ setup.py
@@ -2,27 +2,41 @@
 
 from distutils.core import setup
 
-setup(name='Visual Object Tracking toolkit',
-      version='8.0',
-      description='Perform visual object tracking experiments and analyze results',
-      author='Luka Cehovin Zajc',
-      author_email='luka.cehovin@gmail.com',
-      url='https://github.com/votchallenge/toolkit',
-      packages=['vot'],
-      install_requires=[
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='vot-toolkit',
+    version='8.0',
+    description='Perform visual object tracking experiments and analyze results',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Luka Cehovin Zajc',
+    author_email='luka.cehovin@gmail.com',
+    url='https://github.com/votchallenge/toolkit',
+    packages=['vot'],
+    install_requires=[
+        "vot-trax>=3.0",
         "tqdm==4.32",
         "numpy>=1.16",
         "opencv-python>=4.0",
-        "trax>=3.0",
         "six",
         "plotly>=4.5",
         "dash>=1.8",
         "pylatex>=1.3",
-        "jsonschema>=3.2"],
+        "jsonschema>=3.2",
+        "pyYAML>=5.3",
+        "matplotlib>=3.1",
+        "Pillow>=7.0",
+        "numba>=0.48",
+        "requests>=2.22"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
     ],
     python_requires='>=3.4',
     entry_points={
