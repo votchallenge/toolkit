24985236b9e45114b4c869c9f69a344c24a452c2
Luka Cehovin <luka.cehovin@gmail.com>
2020-11-24 14:39:30 +0000

    More grid methods

--- vot/utilities/data.py
+++ vot/utilities/data.py
@@ -1,16 +1,27 @@
 import functools
+from typing import Tuple, Union, Iterable
+import unittest
 
+import numpy as np
 
 class Grid(object):
 
-    def __init__(self, size: tuple):
+    @staticmethod
+    def scalar(obj):
+        grid = Grid(1,1)
+        grid[0, 0] = obj
+        return grid
+
+    def __init__(self, *size):
+        assert len(size) > 0
         self._size = size
         self._data = [None] * functools.reduce(lambda x, y: x * y, size)
 
     def _ravel(self, pos):
         if not isinstance(pos, tuple):
             pos = (pos, )
-        assert(len(pos) == len(self._size))
+        assert len(pos) == len(self._size)
+
         raveled = 0
         row = 1
         for n, i in zip(reversed(self._size), reversed(pos)):
@@ -19,6 +30,13 @@ class Grid(object):
             raveled = i * row + raveled
             row = row * n
         return raveled
+
+    def _unravel(self, index):
+        unraveled = []
+        for n in reversed(self._size):
+            unraveled.append(index % n)
+            index = index // n
+        return tuple(reversed(unraveled))
 
     def __str__(self):
         return str(self._data)
@@ -45,16 +63,48 @@ class Grid(object):
     def __iter__(self):
         return iter(self._data)
 
+    def cell(self, *i):
+        return Grid.scalar(self[i])
+
     def column(self, i):
         assert self.dimensions == 2
-        column = Grid((self.size()[0], ))
+        column = Grid(1, self.size()[0])
         for j in range(self.size()[0]):
-            column[j] = self[j, i]
+            column[0, j] = self[j, i]
         return column
 
     def row(self, i):
         assert self.dimensions == 2
-        row = Grid((self.size()[1], ))
+        row = Grid(self.size()[1], 1)
         for j in range(self.size()[1]):
-            row[j] = self[i, j]
+            row[j, 0] = self[i, j]
         return row
+
+    def foreach(self, cb) -> "Grid":
+        result = Grid(*self._size)
+
+        for i, x in enumerate(self._data):
+            a = self._unravel(i)
+            result[a] = cb(x, *a)
+
+        return result
+
+class TestGrid(unittest.TestCase):
+
+    def test_foreach1(self):
+
+        a = Grid(5, 3)
+
+        b = a.foreach(lambda x, i, j: 5)
+
+        self.assertTrue(all([x == 5 for x in b]), "Output incorrect")
+
+    def test_foreach2(self):
+
+        a = Grid(5, 6, 3)
+
+        b = a.foreach(lambda x, i, j, k: k)
+
+        reference = [x % 3 for x in range(len(a))]
+
+        self.assertListEqual(list(b), reference)
