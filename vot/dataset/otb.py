85fdf863be87141f0788252ff823e21e810853e6
Luka Cehovin <luka.cehovin@gmail.com>
2021-08-18 14:17:01 +0100

    Fixing OTB dataset loading

--- vot/dataset/otb.py
+++ vot/dataset/otb.py
@@ -18,7 +18,7 @@ _OTB50_SUBSET = ["Basketball", "Biker", "Bird1", "BlurBody", "BlurCar2", "BlurFa
     "DragonBaby", "Dudek", "Football", "Freeman4", "Girl", "Human3", "Human4", "Human6", "Human9",
     "Ironman", "Jump", "Jumping", "Liquor", "Matrix", "MotorRolling", "Panda", "RedTeam", "Shaking",
     "Singer2", "Skating1", "Skating2_1", "Skating2_2", "Skiing", "Soccer", "Surfer", "Sylvester", "Tiger2",
-    "Trellis", "Walking", "Walking2", "Woman" ]
+    "Trellis", "Walking", "Walking2", "Woman"]
 
 _SEQUENCES = {
     "Basketball": {"attributes": ["IV", "OCC", "DEF", "OPR", "BC"]},
@@ -39,7 +39,7 @@ _SEQUENCES = {
     "Crowds": {"attributes": ["IV", "DEF", "BC"]},
     "David": {"attributes": ["IV", "SV", "OCC", "DEF", "MB", "IPR", "OPR"], "start": 300, "stop": 770},
     "Deer": {"attributes": ["MB", "FM", "IPR", "BC", "LR"]},
-    "Diving": {"attributes": ["SV", "DEF", "IPR"]},
+    "Diving": {"attributes": ["SV", "DEF", "IPR"], "stop": 215},
     "DragonBaby": {"attributes": ["SV", "OCC", "MB", "FM", "IPR", "OPR", "OV"]},
     "Dudek": {"attributes": ["SV", "OCC", "DEF", "FM", "IPR", "OPR", "OV", "BC"]},
     "Football": {"attributes": ["OCC", "IPR", "OPR", "BC"]},
@@ -60,8 +60,8 @@ _SEQUENCES = {
     "Shaking": {"attributes": ["IV", "SV", "IPR", "OPR", "BC"]},
     "Singer2": {"attributes": ["IV", "DEF", "IPR", "OPR", "BC"]},
     "Skating1": {"attributes": ["IV", "SV", "OCC", "DEF", "OPR", "BC"]},
-    "Skating2_1": {"attributes": ["SV", "OCC", "DEF", "FM", "OPR"], "base": "Skating2"},
-    "Skating2_2": {"attributes": ["SV", "OCC", "DEF", "FM", "OPR"], "base": "Skating2"},
+    "Skating2_1": {"attributes": ["SV", "OCC", "DEF", "FM", "OPR"], "base": "Skating2", "groundtruth" : "groundtruth_rect.1.txt"},
+    "Skating2_2": {"attributes": ["SV", "OCC", "DEF", "FM", "OPR"], "base": "Skating2", "groundtruth" : "groundtruth_rect.2.txt"},
     "Skiing": {"attributes": ["IV", "SV", "DEF", "IPR", "OPR"]},
     "Soccer": {"attributes": ["IV", "SV", "OCC", "MB", "FM", "IPR", "OPR", "BC"]},
     "Surfer": {"attributes": ["SV", "FM", "IPR", "OPR", "LR"]},
@@ -73,10 +73,10 @@ _SEQUENCES = {
     "Woman": {"attributes": ["IV", "SV", "OCC", "DEF", "MB", "FM", "OPR"]},
     # OTB-100 sequences
     "Bird2": {"attributes": ["OCC", "DEF", "FM", "IPR", "OPR"]},
-    "BlurCar1": {"attributes": ["MB", "FM"]},
-    "BlurCar3": {"attributes": ["MB", "FM"]},
-    "BlurCar4": {"attributes": ["MB", "FM"]},
-    "Board": {"attributes": ["SV", "MB", "FM", "OPR", "OV", "BC"]},
+    "BlurCar1": {"attributes": ["MB", "FM"], "start": 247},
+    "BlurCar3": {"attributes": ["MB", "FM"], "start": 3},
+    "BlurCar4": {"attributes": ["MB", "FM"], "start": 18},
+    "Board": {"attributes": ["SV", "MB", "FM", "OPR", "OV", "BC"], "zeros": 5},
     "Bolt2": {"attributes": ["DEF", "BC"]},
     "Boy": {"attributes": ["SV", "MB", "FM", "IPR", "OPR"]},
     "Car2": {"attributes": ["IV", "SV", "MB", "FM", "BC"]},
@@ -104,8 +104,8 @@ _SEQUENCES = {
     "Human5": {"attributes": ["SV", "OCC", "DEF"]},
     "Human7": {"attributes": ["IV", "SV", "OCC", "DEF", "MB", "FM"]},
     "Human8": {"attributes": ["IV", "SV", "DEF"]},
-    "Jogging1": {"attributes": ["OCC", "DEF", "OPR"], "base": "Jogging"},
-    "Jogging2": {"attributes": ["OCC", "DEF", "OPR"], "base": "Jogging"},
+    "Jogging1": {"attributes": ["OCC", "DEF", "OPR"], "base": "Jogging", "groundtruth" : "groundtruth_rect.1.txt"},
+    "Jogging2": {"attributes": ["OCC", "DEF", "OPR"], "base": "Jogging", "groundtruth" : "groundtruth_rect.2.txt"},
     "KiteSurf": {"attributes": ["IV", "OCC", "IPR", "OPR"]},
     "Lemming": {"attributes": ["IV", "SV", "OCC", "FM", "OPR", "OV"]},
     "Man": {"attributes": ["IV"]},
@@ -149,22 +149,22 @@ class OTBSequence(BaseSequence):
 
         metadata = _SEQUENCES[self.name]
 
-        channels["color"] = PatternFileListChannel(os.path.join(self._base, "img", "%04d.jpg"),
-            start=metadata.get("start", 1), end=metadata.get("end", None))
-         
+        channels["color"] = PatternFileListChannel(os.path.join(self._base, "img", "%%0%dd.jpg" % metadata.get("zeros", 4)),
+            start=metadata.get("start", 1), end=metadata.get("stop", None))
+
         self._metadata["channel.default"] = "color"
         self._metadata["width"], self._metadata["height"] = six.next(six.itervalues(channels)).size
 
-        groundtruth_file = os.path.join(self._base, "groundtruth_rect.txt")
+        groundtruth_file = os.path.join(self._base, metadata.get("groundtruth", "groundtruth_rect.txt"))
 
         with open(groundtruth_file, 'r') as filehandle:
             for region in filehandle.readlines():
-                groundtruth.append(parse(region))
+                groundtruth.append(parse(region.replace("\t", ",").replace(" ", ",")))
 
         self._metadata["length"] = len(groundtruth)
 
         if not channels["color"].length == len(groundtruth):
-            raise DatasetException("Length mismatch between groundtruth and images")
+            raise DatasetException("Length mismatch between groundtruth and images %d != %d" % (channels["color"].length, len(groundtruth)))
 
         return channels, groundtruth, {}, {}
 
@@ -174,8 +174,6 @@ class OTBDataset(Dataset):
         super().__init__(path)
 
         dataset = _SEQUENCES
-
-        print(len(_OTB50_SUBSET))
 
         if not OTBDataset.check(path):
             raise DatasetException("Unknown dataset format, expected OTB")
