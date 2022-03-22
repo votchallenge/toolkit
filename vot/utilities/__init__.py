b2ccf693fbb8fe61180cd3c5185d66e2c2d203be
Luka Cehovin Zajc <luka.cehovin@gmail.com>
2022-03-22 15:56:43 +0100

    Inherit docstrings

--- vot/utilities/__init__.py
+++ vot/utilities/__init__.py
@@ -3,10 +3,10 @@ import sys
 import csv
 import re
 import hashlib
-import errno
 import logging
 import inspect
 import concurrent.futures as futures
+from functools import wraps
 from logging import Formatter, LogRecord
 
 from numbers import Number
@@ -19,6 +19,52 @@ import colorama
 
 __ALIASES = dict()
 
+class DocInherit(object):
+    """
+    Docstring inheriting method descriptor
+
+    The class itself is also used as a decorator
+    """
+
+    def __init__(self, mthd):
+        self.mthd = mthd
+        self.name = mthd.__name__
+
+    def __get__(self, obj, cls):
+        if obj:
+            return self.get_with_inst(obj, cls)
+        else:
+            return self.get_no_inst(cls)
+
+    def get_with_inst(self, obj, cls):
+
+        overridden = getattr(super(cls, obj), self.name, None)
+
+        @wraps(self.mthd, assigned=('__name__','__module__'))
+        def f(*args, **kwargs):
+            return self.mthd(obj, *args, **kwargs)
+
+        return self.use_parent_doc(f, overridden)
+
+    def get_no_inst(self, cls):
+
+        for parent in cls.__mro__[1:]:
+            overridden = getattr(parent, self.name, None)
+            if overridden: break
+
+        @wraps(self.mthd, assigned=('__name__','__module__'))
+        def f(*args, **kwargs):
+            return self.mthd(*args, **kwargs)
+
+        return self.use_parent_doc(f, overridden)
+
+    def use_parent_doc(self, func, source):
+        if source is None:
+            raise NameError("Can't find '%s' in parents"%self.name)
+        func.__doc__ = source.__doc__
+        return func
+
+doc_inherit = DocInherit 
 
 def import_class(classpath):
     delimiter = classpath.rfind(".")
